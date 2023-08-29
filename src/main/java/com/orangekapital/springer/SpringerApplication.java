eb574e8439738bdde5b6c67c13f3242426a49c81
Olawalesegun <olawalesegun0@gmail.com>
2023-08-29 23:58:15 +0100

    hands on spring

--- src/main/java/com/orangekapital/springer/SpringerApplication.java
+++ src/main/java/com/orangekapital/springer/SpringerApplication.java
@@ -2,12 +2,19 @@ package com.orangekapital.springer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class SpringerApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(SpringerApplication.class, args);
+    }
+    @GetMapping(/home)
+    public String sayHi(){
+        return "Hello ";
     }
 
 }
