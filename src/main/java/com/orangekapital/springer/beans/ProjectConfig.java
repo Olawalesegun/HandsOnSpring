38c8a24a7ccbcb32eea6e983f3f993aa674d885d
Olawalesegun <olawalesegun0@gmail.com>
2023-09-02 20:33:10 +0100

    Creating Beans using @Beans notification

--- src/main/java/com/orangekapital/springer/beans/ProjectConfig.java
+++ src/main/java/com/orangekapital/springer/beans/ProjectConfig.java
@@ -1,4 +1,20 @@
 package com.orangekapital.springer.beans;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
 public class ProjectConfig {
+    @Bean
+    Vehicle vehicle(){
+        var vehicle = new Vehicle();
+        vehicle.setName("Audi 22");
+        return vehicle;
+    }
+    @Bean
+    String hello(){
+        return "Hi Welcome to Segun trying to understand how " +
+                "things work under the hood"+
+                "and trying to stop being a whimp";
+    }
 }
