66c4229fc5a50a3cf6b45510a7c77fc4a645c504
Olawalesegun <olawalesegun0@gmail.com>
2023-09-03 20:51:00 +0100

    adding custome name to @Beans to provide unique identity for when we intend to get Beans

--- src/main/java/com/orangekapital/springer/beans/customeNameForBeans2/ProjectCustomNameConfig.java
+++ src/main/java/com/orangekapital/springer/beans/customeNameForBeans2/ProjectCustomNameConfig.java
@@ -1,4 +1,4 @@
-package com.orangekapital.springer.beans.customeNameForBeans;
+package com.orangekapital.springer.beans.customeNameForBeans2;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Primary;
