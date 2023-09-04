85cb78d6b1cafab71644d017dfc332f8041b8ad6
Olawalesegun <olawalesegun0@gmail.com>
2023-09-04 22:11:55 +0100

    creating beans using xml

--- src/main/java/com/orangekapital/springer/beans/creatingBeansWithXML7/Vehicle.java
+++ src/main/java/com/orangekapital/springer/beans/creatingBeansWithXML7/Vehicle.java
@@ -1,4 +1,14 @@
 package com.orangekapital.springer.beans.creatingBeansWithXML7;
 
 public class Vehicle {
+
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
