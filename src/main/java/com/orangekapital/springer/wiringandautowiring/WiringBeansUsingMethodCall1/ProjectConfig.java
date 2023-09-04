a316a4b4613a30b3a0c110c2f99e296983d4ef19
Olawalesegun <olawalesegun0@gmail.com>
2023-09-04 22:26:57 +0100

    Wiring Beans/creating dependencies using Method call

--- src/main/java/com/orangekapital/springer/wiringandautowiring/WiringBeansUsingMethodCall1/ProjectConfig.java
+++ src/main/java/com/orangekapital/springer/wiringandautowiring/WiringBeansUsingMethodCall1/ProjectConfig.java
@@ -1,2 +1,25 @@
-package com.orangekapital.springer.wiringandautowiring.WiringBeansUsingMethodCall1;public class ProjectConfig {
+package com.orangekapital.springer.wiringandautowiring.WiringBeansUsingMethodCall1;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan(basePackages = "com.orangekapital.springer.wiringandautowiring.WiringBeansUsingMethodCall1")
+public class ProjectConfig {
+
+    @Bean
+    Vehicle vehicle(){
+        var veh = new Vehicle();
+        veh.setName("Anaconda55");
+        return veh;
+    }
+
+    @Bean
+    Person person(){
+        var person = new Person();
+        person.setPersonName("Jibola Balogun");
+        person.setVehicle(vehicle());
+        return person;
+    }
 }
