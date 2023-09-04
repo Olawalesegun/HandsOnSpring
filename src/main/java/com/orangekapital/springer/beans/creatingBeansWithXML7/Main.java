f7e2b01c6bba2faa3bc659f30acc88f141a2b6ba
Olawalesegun <olawalesegun0@gmail.com>
2023-09-04 22:10:15 +0100

    creating beans using xml

--- src/main/java/com/orangekapital/springer/beans/creatingBeansWithXML7/Main.java
+++ src/main/java/com/orangekapital/springer/beans/creatingBeansWithXML7/Main.java
@@ -1,4 +1,13 @@
 package com.orangekapital.springer.beans.creatingBeansWithXML7;
 
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
 public class Main {
+
+    //Best advise to not do this as xml are not used in creating Beans anymore
+    public static void main(String[] args) {
+        var context = new ClassPathXmlApplicationContext("beans.xml");
+        Vehicle veh = context.getBean(Vehicle.class);
+        System.out.println(veh.getName());
+    }
 }
