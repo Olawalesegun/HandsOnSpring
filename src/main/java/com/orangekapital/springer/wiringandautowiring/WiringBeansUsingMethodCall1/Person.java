8a38eaef989c3e1f3c0871e5f1a03df1a9dd17b9
Olawalesegun <olawalesegun0@gmail.com>
2023-09-04 22:28:37 +0100

    Wiring Beans/creating dependencies using Method call

--- src/main/java/com/orangekapital/springer/wiringandautowiring/WiringBeansUsingMethodCall1/Person.java
+++ src/main/java/com/orangekapital/springer/wiringandautowiring/WiringBeansUsingMethodCall1/Person.java
@@ -1,2 +1,23 @@
-package com.orangekapital.springer.wiringandautowiring.WiringBeansUsingMethodCall1;public class Person {
+package com.orangekapital.springer.wiringandautowiring.WiringBeansUsingMethodCall1;
+
+public class Person {
+    private String personName;
+
+    public Vehicle getVehicle() {
+        return vehicle;
+    }
+
+    public void setVehicle(Vehicle vehicle) {
+        this.vehicle = vehicle;
+    }
+
+    private Vehicle vehicle;
+
+    public String getPersonName() {
+        return personName;
+    }
+
+    public void setPersonName(String personName) {
+        this.personName = personName;
+    }
 }
