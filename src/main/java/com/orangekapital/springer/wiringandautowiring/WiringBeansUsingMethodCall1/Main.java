1f823e5c440aef5efa3bd076b10dd644757138d4
Olawalesegun <olawalesegun0@gmail.com>
2023-09-04 22:26:08 +0100

    Wiring Beans/creating dependencies using Method call

--- src/main/java/com/orangekapital/springer/wiringandautowiring/WiringBeansUsingMethodCall1/Main.java
+++ src/main/java/com/orangekapital/springer/wiringandautowiring/WiringBeansUsingMethodCall1/Main.java
@@ -1,4 +1,17 @@
 package com.orangekapital.springer.wiringandautowiring.WiringBeansUsingMethodCall1;
 
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
 public class Main {
+
+    public static void main(String[] args) {
+        var context = new AnnotationConfigApplicationContext(ProjectConfig.class);
+        Vehicle veh = context.getBean(Vehicle.class);
+        System.out.println("Vehicle name is " + veh.getName());
+
+        Person person = context.getBean(Person.class);
+        System.out.println("Person name is " + person.getPersonName());
+        System.out.println(person.getVehicle());
+
+    }
 }
