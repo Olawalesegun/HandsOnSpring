13606809fc462ba5aca4983df23f6ad7ffeb6859
Olawalesegun <olawalesegun0@gmail.com>
2023-09-05 20:52:50 +0100

    Wiring Beans/creating dependencies using Method parameter

--- src/main/java/com/orangekapital/springer/wiringandautowiring/wiringBeansUsingMethodParameters2/Vehicle.java
+++ src/main/java/com/orangekapital/springer/wiringandautowiring/wiringBeansUsingMethodParameters2/Vehicle.java
@@ -1,2 +1,21 @@
-package com.orangekapital.springer.wiringandautowiring.wiringBeansUsingMethodParameters2;public class Vehicle {
+package com.orangekapital.springer.wiringandautowiring.wiringBeansUsingMethodParameters2;
+
+public class Vehicle {
+    private String name;
+    public Vehicle(){
+        System.out.println("This vehicle is " + getName());
+    }
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "Vehicle{" +
+                "name='" + name + '\'' +
+                '}';
+    }
 }
