b79263f950c57dfa65896395b7ef0fe4f556ebe8
Olawalesegun <olawalesegun0@gmail.com>
2023-09-05 20:52:38 +0100

    Wiring Beans/creating dependencies using Method parameter

--- src/main/java/com/orangekapital/springer/wiringandautowiring/wiringBeansUsingMethodParameters2/ProjectConfig.java
+++ src/main/java/com/orangekapital/springer/wiringandautowiring/wiringBeansUsingMethodParameters2/ProjectConfig.java
@@ -1,4 +1,23 @@
 package com.orangekapital.springer.wiringandautowiring.wiringBeansUsingMethodParameters2;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan(basePackages = "com.orangekapital.springer.wiringandautowiring.wiringBeansUsingMethodParameters2")
 public class ProjectConfig {
+    @Bean
+    Vehicle vehicle(){
+        var vehicle = new Vehicle();
+        vehicle.setName("Nissan");
+        return vehicle;
+    }
+    @Bean
+    Person person(Vehicle vehicle){
+        var person = new Person();
+        person.setName("Jibola Dabo");
+        person.setVehicle(vehicle);
+        return person;
+    }
 }
